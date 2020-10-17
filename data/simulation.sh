0dd46501ac927efeddcd1f1b2153d9807ca042a0
BobbyNie <bobbynie@139.com>
2020-10-17 08:59:51 +0800

    add  simulation data

---
+++ data/simulation.sh
@@ -0,0 +1,11 @@
+
+ymd=`date '+%Y%m%d'`;
+#取上一分钟时间。 
+ihslogfile=443_ssl_access_$ymd.log
+
+echo >$ihslogfile;
+while(true); do
+    hmColon=`perl -MPOSIX -le 'print strftime " %H:%M:",localtime(time)'`
+    cat simulationdata.log |grep "$hmColon" >> $ihslogfile;
+    sleep 59;
+done;
